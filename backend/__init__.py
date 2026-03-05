a8f0c24a921e3ef73807112edc677e21992d7f97
kibeterick <kibeterick57@gmail.com>
2026-03-05 14:37:41 +0300

    Fix: Comment out Celery import for PythonAnywhere deployment

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,8 +3,9 @@ Backend package initialization
 Ensures Celery app is loaded when Django starts
 """
 
+# Celery import commented out - not installed on PythonAnywhere
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from core_blood_system.celery import app as celery_app
+# from core_blood_system.celery import app as celery_app
 
-__all__ = ('celery_app',)
+# __all__ = ('celery_app',)
